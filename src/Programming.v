40e2b55a5f49a33497a954250514ea286a8a17dd
gregory malecha <gmalecha@cs.harvard.edu>
2012-12-14 23:47:58 -0500

    Programming.

--- src/Programming.v
+++ src/Programming.v
@@ -450,10 +450,12 @@ Hint Extern 1 (?specs _ = Some _) =>
     | [ H : specs _ = Some _ |- _ ] => apply (specs_cong H); congruence
   end.
 
-Lemma use_himp : forall pc state specs (P Q : hprop pc state nil), himp specs P Q
+Lemma use_himp : forall specs (P Q : hprop),
+  himp P Q
   -> forall s m, interp specs (P s m)
     -> interp specs (Q s m).
-  intros; apply (Imply_sound (H _ _)); auto.
+  unfold himp. intuition. eapply Imply_E. 
+  2: eapply H0. auto.
 Qed.
 
 Lemma Imply_refl : forall pc state specs (P : PropX pc state),
