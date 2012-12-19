5f138741dea15f01842d942583b49791bd441434
gregory malecha <gmalecha@cs.harvard.edu>
2012-12-19 11:40:36 -0500

    getting rid of Bedrock specific stuff.

--- examples/comparison/list.h
+++
@@ -1,4 +0,0 @@
-typedef struct list {
-  unsigned data;
-  struct list *next;
-} *list;
--- examples/comparison/ocaml_driver.ml
+++
@@ -1,8 +0,0 @@
-open Ocaml_data
-open Ocaml_server
-
-let main () =
-  let output = server cmd data in
-  List.iter (Printf.printf "%d\n") output
-
-let () = main ()
